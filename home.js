2c63ed0b03a9389185eba5596c48b40ea7499534
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-02-10 23:43:39 +0530

    1st

---
+++ home.js
@@ -0,0 +1,23 @@
+console.log('Hi How Are You');
+
+document.write('Hi i can fly');
+
+var a = "apple";
+
+// alert('is any proiblem ? ');
+
+// var b = prompt('what is your age');
+
+// var age = prompt('what is your age'); 
+
+// console.log(age);
+
+// document.getElementById('someText').innerHTML = age;
+
+var num1 = 10;
+
+num1 = num1 + 1;
+
+console.log(num1);
+
+//function
