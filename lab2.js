5639b75540e1f36daac2c3171e61679ed45cd087
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-02-18 19:49:48 +0530

    8th comit

--- lab2.js
+++ lab2.js
@@ -16,3 +16,30 @@ let getValue = function (callback) {
 }
 
 console.log(getValue());
+
+function Vehicle(type) {
+    this.type = type;
+    Vehicle.VehicleCount++;
+}
+Vehicle.VehicleCount = 0;
+console.log(Vehicle.VehicleCount);
+const vehicle = new Vehicle("Toyota");
+console.log(vehicle.type);
+console.log(Vehicle.VehicleCount);
+Vehicle.prototype.drive = function () {
+    console.log(this.type + " is driving");
+};
+function Car(type) {
+    Vehicle.call(this, type);
+}
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+Car.prototype.balanceWheels = function () {
+    console.log("Wheels are balanced");
+};
+const car = new Car("Honda");
+vehicle.drive();
+car.drive();
+car.balanceWheels();
+console.log(Vehicle.VehicleCount);
+console.log(car.type);
