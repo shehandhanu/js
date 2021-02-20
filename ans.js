dbc0373e51228d3928e5d8d4a9d75762ab4eba9e
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-02-20 15:04:57 +0530

    9th

---
+++ ans.js
@@ -0,0 +1,23 @@
+
+taxPer = 0.2;
+
+let tax = (function () {
+    let amt = 1000;
+    let taxPer = 0.3;
+    return function () {
+        amt = (this.taxPer * amt) + amt;
+        return amt;
+    }
+}
+)();
+
+document.write(tax());
+
+///////////////////////////////////
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        document.write(JSON.stringify(data))
+    })
+    .catch(error => console.log(error));
