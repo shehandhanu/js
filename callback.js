5ad6a8ebb93a868aa8c133cdde88fdae75bc83b1
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-02-22 16:52:16 +0530

    12th

--- callback.js
+++ callback.js
@@ -14,17 +14,17 @@ console.log('End');
 
 /////////////////////////////////
 
-// console.log('Started');
+console.log('Started');
 
-// function getdata(id, callback) {
-//     setTimeout(() => {
-//         console.log('Set Timeout');
-//         callback({ items: [1, 2, 3, 4, 5] });
-//     }, 3000);
-// };
+function getdata(id, callback) {
+    setTimeout(() => {
+        console.log('Set Timeout');
+        callback({ items: [1, 2, 3, 4, 5] });
+    }, 3000);
+};
 
-// const items = getdata(1, items => {
-//     console.log(items);
-// });
+const items = getdata(1, items => {
+    console.log(items);
+});
 
-// console.log('End');
+console.log('End');
